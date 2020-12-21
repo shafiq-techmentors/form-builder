a37f2c05fd54aa197f9f2ca12c95f64d49b90b8c
shafiq-techmentors <hello.shafiqkhan@gmail.com>
2020-12-21 10:11:23 +0500

    First Commit

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import UserCreate from "./user-create";
+import UserList from "./user-list";
+import SignIn from "./login";
+import Dashboard from "./dashboard";
+import StartScreen from "./start-screen";
+
+export default {
+  UserCreate,
+  SignIn,
+  UserList,
+  Dashboard,
+  StartScreen,
+};
---
+++ src/components/start-screen/index.js
@@ -0,0 +1,162 @@
+import React, { useState } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Modal from "components/shared/modal";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
+import Container from "@material-ui/core/Container";
+import Box from "@material-ui/core/Box";
+import Copyright from "components/shared/copyright";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import { useStyles } from "./Style";
+import Logo from "assets/images/logo.png";
+import TextField from "@material-ui/core/TextField";
+import { withRouter } from "react-router-dom";
+
+const StartScreen = (props) => {
+  const classes = useStyles();
+  const [formValue, setFormValue] = useState({
+    formName: "",
+  });
+
+  const handleChange = (e) => {
+    setFormValue({
+      ...formValue,
+      formName: e.target.value,
+    });
+    console.log("form name....", e.target.value);
+  };
+
+  const handleSubmit = () => {
+    localStorage.setItem(
+      "__",
+      JSON.stringify({
+        formName: formValue.formName,
+      })
+    );
+    props.history.push("/admin-dashboard");
+    console.log("form name noted.....", formValue.formName);
+  };
+  const handleCancel = () => {};
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <AppBar
+        position="static"
+        color="default"
+        elevation={0}
+        className={classes.appBar}
+      >
+        <Toolbar className={classes.toolbar}>
+          <Typography
+            variant="h6"
+            color="inherit"
+            noWrap
+            className={classes.toolbarTitle}
+          >
+            <img src={Logo} alt="logo" style={{ width: "150px" }} />
+          </Typography>
+          <nav>
+            <Link
+              variant="button"
+              color="textPrimary"
+              href="#"
+              className={classes.link}
+            >
+              Documentations
+            </Link>
+            <Link
+              variant="button"
+              color="textPrimary"
+              href="#"
+              className={classes.link}
+            >
+              Features
+            </Link>
+            <Link
+              variant="button"
+              color="textPrimary"
+              href="#"
+              className={classes.link}
+            >
+              Support
+            </Link>
+          </nav>
+          <Button
+            href="#"
+            color="primary"
+            variant="outlined"
+            className={classes.link}
+          >
+            Login
+          </Button>
+        </Toolbar>
+      </AppBar>
+      {/* Hero unit */}
+      <Container maxWidth="sm" component="main" className={classes.heroContent}>
+        <Typography
+          component="h1"
+          variant="h2"
+          align="center"
+          className={classes.topHeading}
+          gutterBottom
+        >
+          Let's get started!
+        </Typography>
+        <Typography
+          variant="h5"
+          align="center"
+          color="textSecondary"
+          component="p"
+        >
+          The first step is to create a form. Once you create a form then you
+          can export and save in local directory. When you need the same
+          designed form anywhere in the project, you can simply import the form
+        </Typography>
+      </Container>
+
+      <Grid container direction="row" justify="center" alignItems="center">
+        <Grid item>
+          {/* <Button align="center" variant="contained" color="primary">
+            Create a Project
+          </Button> */}
+          <Modal
+            title="Create a Form"
+            btnText="Create a Form"
+            btnColor="primary"
+            btnVariant="contained"
+            btnAlign="center"
+            footeBtn1="Create"
+            footerBtn2="Cancel"
+            bodyText="Provide meaninggul name to the form and save"
+            onSubmit={handleSubmit}
+            onCancel={handleCancel}
+          >
+            <form className={classes.root} noValidate autoComplete="off">
+              <TextField
+                id="standard-basic"
+                label="Form Name"
+                value={formValue.formName}
+                onChange={handleChange}
+                fullWidth
+              />
+            </form>
+          </Modal>
+        </Grid>
+      </Grid>
+
+      {/* Footer */}
+      <Container maxWidth="md" component="footer" className={classes.footer}>
+        <Box mt={5}>
+          <Copyright />
+        </Box>
+      </Container>
+      {/* End footer */}
+    </React.Fragment>
+  );
+};
+
+export default withRouter(StartScreen);
